9c3e4ac84bbb19c187dc43aa7a009b4fb0abb44a
bilboandyoda <jordboudreau@gmail.com>
2018-01-07 16:20:19 -0800

    v1

--- www/code.js
+++ www/code.js
@@ -21,15 +21,17 @@ $(document).ready(function(){
             var $window = $(window),
                 $va1 = $('#va');
                 $va2 = $('#va2');
-
+                $teamshit = $('#teamshit')
             function resize() {
                 if ($window.width() < 768) {
                     $va2.removeClass('vertical-align')
+                    $teamshit.removeClass('lefty');
                     return $va1.removeClass('vertical-align');
                 }
 
                 $va1.addClass('vertical-align');
                 $va2.addClass('vertical-align');
+                $teamshit.addClass('lefty');
                 }
     
                 $window
