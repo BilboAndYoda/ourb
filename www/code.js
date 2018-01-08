cfc8896ee1d2f48a1ebfbd07320e66784d87ebbe
bilboandyoda <jordboudreau@gmail.com>
2018-01-07 16:49:53 -0800

    fix development

--- www/code.js
+++ www/code.js
@@ -21,17 +21,20 @@ $(document).ready(function(){
             var $window = $(window),
                 $va1 = $('#va');
                 $va2 = $('#va2');
-                $teamshit = $('#teamshit')
+                $teamshit = $('#teamshit');
+                $dev = $('#dev');
             function resize() {
                 if ($window.width() < 768) {
                     $va2.removeClass('vertical-align')
                     $teamshit.removeClass('lefty');
+                    $dev.addClass('development')
                     return $va1.removeClass('vertical-align');
                 }
 
                 $va1.addClass('vertical-align');
                 $va2.addClass('vertical-align');
                 $teamshit.addClass('lefty');
+                $dev.removeClass('development');
                 }
     
                 $window
